5be29896a88067244742077e9d99fa799f820a02
Disha Umarwani <dhu200@nyu.edu>
2018-04-06 06:00:11 -0400

    Update run_optimized.sh

--- run_optimized.sh
+++ run_optimized.sh
@@ -1,2 +1,2 @@
-rm ./output/sessionization.txt
+rm ./output/sessionization_optimi.txt
 python ./src/sessionization_optimized.py ./input/log.csv ./input/inactivity_period.txt ./output/sessionization_optimi.txt
