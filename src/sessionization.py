655b3333d62318ccb06715a2bba4cd16e91e1cf5
dishaumarwani <dhu200@nyu.edu>
2018-04-06 02:45:05 -0400

    sessionization.py

--- src/sessionization.py
+++ src/sessionization.py
@@ -7,94 +7,130 @@ def inactivity_period(arg):
     with open(arg) as st:
         for line in st:
             i = int(line)
-    st.close()
+        st.close()
+    #Session time should range between 1 and 86400
     if(i>1 and i<86400):
         return i
     else:
         print("Incorrect session activity period")
 
+def process_line(session_dict, row, ip_in, date_c_in, time_c_in, current_timestamp):
+    ip = row[ip_in]
+    date_time_c = row[date_c_in]+' '+row[time_c_in]
+    dt = datetime.strptime(date_time_c, "%Y-%m-%d %H:%M:%S")
 
-#check if any timestamp has expired
-def expired_timestamp(curr_active_session, current_timestamp, st):
-    for time_stamp in list(curr_active_session):
-        if((current_timestamp - time_stamp).seconds >= st):
-            return time_stamp
+    #Adds ip address to current active sessions if not exist
+    #maintains count of documents requested
+    if(ip in curr_active_sessions):
+        #maintain a count of each time an ip makes a request
+        session_dict[ip]['count'] += 1
 
-#check if ip already present in a timestamp
-def find_ip_address(current_active_session, ip):
-    for i in list(current_active_session):
-        if(ip in list(current_active_session[i])):
-            return i
-    return None
+        #update the most recent session time
+        session_dict[ip]['start_time_stp'] = dt
 
-#writing the output to file at the end of an active session
-def write_output(curr_active_sessions,out_file, et=None):
-    with open(out_file, "a") as fo:
-        if(et):
-            print(curr_act_sessions[et])
-            for i in list(curr_act_sessions[et]):
-                session_time = (datetime.strptime(curr_act_sessions[et][i]['end_time'], "%Y-%m-%d %H:%M:%S")\
-                                -datetime.strptime(curr_act_sessions[et][i]['start_time'], "%Y-%m-%d %H:%M:%S")).seconds + 1
-                wr = i+','+ curr_act_sessions[et][i]['start_time']+ ','+\
-                curr_act_sessions[et][i]['end_time']+','+str(session_time)+','+str(curr_act_sessions[et][i]['count'])+'\n'
-                fo.write(wr)
+    else:
+        #if the ip session is starting
+        session_dict[ip] = {}
 
-            curr_act_sessions.pop(et)        
-        else:        
-            for i in list(curr_act_sessions):
-                for j in list(curr_act_sessions[i]):
-                    session_time = (datetime.strptime(curr_act_sessions[i][j]['end_time'], "%Y-%m-%d %H:%M:%S")\
-                                    -datetime.strptime(curr_act_sessions[i][j]['start_time'], "%Y-%m-%d %H:%M:%S")).seconds + 1
-                    wr = j+','+ curr_act_sessions[i][j]['start_time']+','+\
-                    curr_act_sessions[i][j]['end_time']+','+ str(session_time)+','+\
-                    str(curr_act_sessions[i][j]['count'])+'\n'
-                    fo.write(wr)
-            curr_act_sessions.pop(i)
-    fo.close()
+        #maintain a count variable for no. of requests
+        session_dict[ip]['count'] = 1
+
+        #session start time
+        session_dict[ip]['start_date'] = date_time_c
+
+        #this is updated each time session renews
+        session_dict[ip]['start_time_stp'] = dt
+
+        #this is updates each time a session renews
+        session_dict[ip]['end_date'] = date_time_c
+
+    #This will be true after every second
+    #All the active sessions will be checked if they have expired
+    if(current_timestamp != date_time_c):
+
+        write_output(session_dict, out_file, dt)
         
+        #time will be updated after every second
+        current_timestamp = date_time_c
+    return session_dict, current_timestamp
+
+def write_output(curr_active_sessions, out_file, dt):
+
+    #open file in append mode
+    with open(out_file, "a") as fo:
+        
+        #The ips are will be printed in order of their start time
+        #This condition will hold for all the ip that are written if a user session ends
+        if(dt !=None):
+            for i in list(curr_active_sessions):
+                
+                #check if the period of inactivity is greater than 2 seconds
+                if((dt - curr_active_sessions[i]['start_time_stp']).seconds > 2):
+                    
+                    #find the total time of session time
+                    st = datetime.strptime(curr_active_sessions[i]['start_date'], "%Y-%m-%d %H:%M:%S")
+                    session_time = (curr_active_sessions[i]['start_time_stp'] - st).seconds
+                    
+                    #write the output to the file
+                    wr = i+','+curr_active_sessions[i]['start_date']+','+ curr_active_sessions[i]['end_date']+','+str(session_time+1)+',' \
+                        +str(curr_active_sessions[i]['count'])+'\n'
+                    fo.write(wr)
+
+                    curr_active_sessions.pop(i)
+
+        #This condition holds for ip address when end of file is reached and user session does not end
+        else:
+            for i in list(curr_active_sessions):
+
+                #find the total time of session time
+                st = datetime.strptime(curr_active_sessions[i]['start_date'], "%Y-%m-%d %H:%M:%S")
+                session_time = (curr_active_sessions[i]['start_time_stp'] - st).seconds
+
+                #write the output to the file
+                wr = i+','+curr_active_sessions[i]['start_date']+','+ curr_active_sessions[i]['end_date']+','+str(session_time+1)+',' \
+                        +str(curr_active_sessions[i]['count'])+'\n'
+                fo.write(wr)
+        fo.close()
+
 if __name__ == "__main__":
+
+    #find session time
     st = inactivity_period(sys.argv[2])
+    #input and file will be given as argument
     input_file = sys.argv[1]
     out_file = sys.argv[3]
+    
+    #open file in read mode
     with open(input_file, "r") as f:
-        next(f)
-        spamreader = csv.reader(f, delimiter=',', quotechar="'")
-        curr_act_sessions = dict()
-        current_timestamp = 0
-        
-        for row in spamreader:
-            #extract all the required fields
-            ip = row[0]
-            date_time_c = row[1]+' '+row[2]
-            dt = datetime.strptime(date_time_c, "%Y-%m-%d %H:%M:%S")
-            document = str(row[4])+' '+str(row[5])+' '+str(row[6])
-            
-            #checks in both the timestamps if ip address is present
-            tm = find_ip_address(curr_act_sessions, ip)
 
-            if(tm != None):    
-                curr_act_sessions[tm][ip]['count'] += 1
-                curr_act_sessions[tm][ip]['end_time'] = date_time_c
-                if(tm != dt):
-                    if(current_timestamp != dt):
-                        if(dt not in curr_act_sessions):
-                            curr_act_sessions[dt] = {}
-                        curr_act_sessions[dt][ip] = curr_act_sessions[tm][ip]
-                        curr_act_sessions[tm].pop(ip)        
-            else:
-                if(current_timestamp != dt):
-                    curr_act_sessions[dt] = {}
-                curr_act_sessions[dt][ip] = {}
-                curr_act_sessions[dt][ip]['start_time'] = date_time_c
-                curr_act_sessions[dt][ip]['end_time'] = date_time_c
-                curr_act_sessions[dt][ip]['count'] = 1
-                
-            #This will be true after every second
-            #All the timestamps mainly previous two will be compared if they have expired.
-            if(current_timestamp != dt):
-                et = expired_timestamp(curr_act_sessions, dt, st)
-                if(et):
-                    write_output(curr_act_sessions,out_file, et)
-                current_timestamp = dt
-        f.close()
-    write_output(curr_act_sessions, out_file, None)
+        #extract all the required fields, read it using csv reader to avoid mistaken splitting by commas in the data fields
+        #ip,date,time,cik,accession,extention
+        header = list(csv.reader([next(f)], delimiter=',', quotechar="'"))[0]
+        ip_in = header.index('ip')
+        time_c_in = header.index('time')
+        date_c_in = header.index('date')
+        row = list(csv.reader([next(f)], delimiter=',', quotechar="'"))[0]
+
+        #maintain a dictionary of active sessions
+        curr_active_sessions = dict()
+
+        #Initialize the current time to any random integer
+        current_timestamp = 0
+
+        try:
+            while(row != ''):
+
+                #process each line in file in a streaming manner
+                curr_active_sessions, current_timestamp = process_line(curr_active_sessions, row, ip_in, date_c_in, time_c_in, current_timestamp)
+
+                #read the next row
+                row = list(csv.reader([next(f)], delimiter=',', quotechar="'"))[0]
+        except:
+            pass
+    f.close()
+
+    #write the remaining logs into file
+    write_output(curr_active_sessions, out_file, None)    
+
+
+
