2624ff4caad22b5edd67fb68d9622e4db90d5443
dishaumarwani <dhu200@nyu.edu>
2018-04-06 05:27:10 -0400

    comments

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # EDGAR_Streaming_Log_Processing
 Streaming processing of log file
+
+# Contents
+1. [Preference of Datastructure](README.md#Preferenceof Datastructure)
+2. [Preference of Algorithm](README.md#Preferenceof Algorithm)
+3. [Required Libraries and Environment](README.md#Required Libraries and Environment)
+4. [Implementation Details](README.md#Implementation Details)
+5. [Conclusion](README.md#Conclusion)
+
+#Preference of Datastructure
+
+Data Structure: Due to duck typing nature of python variable data types are not specified.
+
+For storing active sessions an associative array also known as a dictionary is used.
+
+The dictionary structure at any instance of time will be:
+{ip : {
+        'start_time' : string with date and time signifying the first request time
+        'end_time' : string with date and time signifying the last request time
+        'count'    : integer
+        'start_time_stp' : datetime object signifying the last request time
+    }
+
+}
+
+A dictionary makes the retrieval time very fast approximately O(1).
+All the ips are appened in order of their start time.
+
+Another more efficient way would have been to make an index on start_time_sp. 
+This reduces the number of comparisons extensively to a value of 2 versus the worst case comparison of n
+each time there is a change in time encountered. The distadvantage of this was that it required a sort by 
+start time each time the output needs to be written to file which would wash off its gain in time complexity.
+
+#Preference of Algorithm
+
+1. processes each line in the document first extracts the required fields
+
+2. checks if current_timestamp is not equal to time in current log
+if not equal calls the write function
+
+4. write function checks if the inactivity period is greater than mentioned
+and writes the ip to output file
+
+3. checks if the ip address is already present in active sessions
+updates count and start_time_stp
+
+4. creates a new ip key if ip not in active session dictionary and updates all its nested keys as required.
+
+5. writes all the ip left after processing the last line to the file
+
+#Required Libraries and Environment
+
+For the purpose of simplicity only python three libraries have been used
+1. sys
+2. csv 
+3. datetime
+
+The environment reuired for the script to run is Python 3.6+
+
+#Implementation Details
+The code has been implemented in a modular manner in a simple file using functional programming paradigms 
+
+There are 6 functions
+* `inactivity_period` : reads the input file and derives the inactivity period
+* `parse_time` : converts a string to datetime object
+* `time_difference` : calculates difference between two time periods
+* `process_line` : code runs for all lines in the log file parsing the input and then processing it according to the algorithm
+* `write_output` : writes the output to a text file
+* `main function` : maintains the flow of execution
+
+The code handles exceptions at each stage and is robust to errors.
+
+#Conclusion
+
--- src/sessionization.py
+++ src/sessionization.py
@@ -2,26 +2,88 @@ import sys
 import csv
 from datetime import datetime, date, time
 
-#inactivity period
 def inactivity_period(arg):
-    with open(arg) as st:
-        for line in st:
-            i = int(line)
-        st.close()
-    #Session time should range between 1 and 86400
-    if(i>1 and i<86400):
-        return i
-    else:
-        print("Incorrect session activity period")
+    """
+        :param argv: file_name as string
+        :return: session time as int
+    """
+    try:
+        with open(arg, 'r') as f:
+            i = int(next(f))
+            f.close()
+        #Session time should range between 1 and 86400
+        if(i>1 and i<86400):
+            return i
+        else:
+            return "Incorrect session activity period"
+    except IOError:
+        return "There was an error opening the file!"
 
-def process_line(session_dict, row, ip_in, date_c_in, time_c_in, current_timestamp):
+def parse_time(tm):
+    """
+        :param tm: time as string
+        :return: time as datetime object
+    """
+    try:
+        tm = datetime.strptime(tm, "%Y-%m-%d %H:%M:%S")
+    except:
+        try:
+            tm = datetime.strptime(tm, "%y-%m-%d %H:%M:%S")
+        except:
+            try:
+                tm = datetime.strptime(tm, "%m-%d-%y %H:%M:%S")
+            except:
+                tm = datetime.datetime.now()
+    return tm
+
+def time_difference(start, end):
+    """
+        Finds difference between two datetime objects in seconds
+        :param start: datetime object
+        :param end: datetime object
+        :return: time in seconds as int
+
+    """
+    sec = (end - start).seconds
+    return sec
+
+
+def process_line(session_dict, row, ip_in, date_c_in, time_c_in, current_timestamp, st):
+    """
+        processes each line in the document
+        1. extracts the required fields
+
+        2. checks if current_timestamp is not equal to time in current log
+            if not equal calls the write function
+        
+        3. checks if the ip address is already present in active sessions
+            updates count and start_time_stp
+
+        4. creates a new ip key if ip not in active session dictionary
+
+        :param session_dict: associative array/dictionary having records of all active sessions
+        :param row: list of values to be processed
+        :param ip_in: index of the ip address
+        :param date_c_in: index of date
+        :param time_c_in: index of time
+        :param current_timestamp: value of date time of last timestamp
+        :return session_dict: associative array of updated session_dict with active sessions
+    """
     ip = row[ip_in]
     date_time_c = row[date_c_in]+' '+row[time_c_in]
-    dt = datetime.strptime(date_time_c, "%Y-%m-%d %H:%M:%S")
+    dt = parse_time(date_time_c)
+
+    #This will be true after every second
+    #All the active sessions will be checked if they have expired
+    if(current_timestamp != date_time_c):
+        write_output(session_dict, out_file, dt, st)
+        
+        #time will be updated after every second
+        current_timestamp = date_time_c
 
     #Adds ip address to current active sessions if not exist
     #maintains count of documents requested
-    if(ip in curr_active_sessions):
+    if(ip in active_sessions):
         #maintain a count of each time an ip makes a request
         session_dict[ip]['count'] += 1
 
@@ -44,51 +106,51 @@ def process_line(session_dict, row, ip_in, date_c_in, time_c_in, current_timesta
         #this is updates each time a session renews
         session_dict[ip]['end_date'] = date_time_c
 
-    #This will be true after every second
-    #All the active sessions will be checked if they have expired
-    if(current_timestamp != date_time_c):
 
-        write_output(session_dict, out_file, dt)
-        
-        #time will be updated after every second
-        current_timestamp = date_time_c
     return session_dict, current_timestamp
 
-def write_output(curr_active_sessions, out_file, dt):
-
+def write_output(active_sessions, out_file, dt, st):
+    """
+       :param active_sessions: associative array/dictionary of all active ip sessions
+       :param out_file: filepath name as string
+       :param dt current time
+    """
     #open file in append mode
     with open(out_file, "a") as fo:
-        
+
         #The ips are will be printed in order of their start time
         #This condition will hold for all the ip that are written if a user session ends
         if(dt !=None):
-            for i in list(curr_active_sessions):
+            for i in list(active_sessions):
                 
                 #check if the period of inactivity is greater than 2 seconds
-                if((dt - curr_active_sessions[i]['start_time_stp']).seconds > 2):
+
+                if(time_difference(active_sessions[i]['start_time_stp'], dt) > st):
                     
                     #find the total time of session time
-                    st = datetime.strptime(curr_active_sessions[i]['start_date'], "%Y-%m-%d %H:%M:%S")
-                    session_time = (curr_active_sessions[i]['start_time_stp'] - st).seconds
+                    ses_end_time = parse_time(active_sessions[i]['start_date'])
+                    session_time = time_difference(ses_end_time, active_sessions[i]['start_time_stp'])
+                    
                     
                     #write the output to the file
-                    wr = i+','+curr_active_sessions[i]['start_date']+','+ curr_active_sessions[i]['end_date']+','+str(session_time+1)+',' \
-                        +str(curr_active_sessions[i]['count'])+'\n'
+                    wr = i+','+active_sessions[i]['start_date']+','+ active_sessions[i]['end_date']+','+str(session_time+1)+',' \
+                        +str(active_sessions[i]['count'])+'\n'
                     fo.write(wr)
 
-                    curr_active_sessions.pop(i)
+                    #remove the ip from current active session
+                    active_sessions.pop(i)
 
         #This condition holds for ip address when end of file is reached and user session does not end
         else:
-            for i in list(curr_active_sessions):
+            for i in list(active_sessions):
 
                 #find the total time of session time
-                st = datetime.strptime(curr_active_sessions[i]['start_date'], "%Y-%m-%d %H:%M:%S")
-                session_time = (curr_active_sessions[i]['start_time_stp'] - st).seconds
+                ses_end_time = parse_time(active_sessions[i]['start_date'])
+                session_time = time_difference(ses_end_time, active_sessions[i]['start_time_stp'])
 
                 #write the output to the file
-                wr = i+','+curr_active_sessions[i]['start_date']+','+ curr_active_sessions[i]['end_date']+','+str(session_time+1)+',' \
-                        +str(curr_active_sessions[i]['count'])+'\n'
+                wr = i+','+active_sessions[i]['start_date']+','+ active_sessions[i]['end_date']+','+str(session_time+1)+',' \
+                        +str(active_sessions[i]['count'])+'\n'
                 fo.write(wr)
         fo.close()
 
@@ -96,41 +158,49 @@ if __name__ == "__main__":
 
     #find session time
     st = inactivity_period(sys.argv[2])
-    #input and file will be given as argument
-    input_file = sys.argv[1]
-    out_file = sys.argv[3]
-    
-    #open file in read mode
-    with open(input_file, "r") as f:
+    if(type(st) == int):
 
-        #extract all the required fields, read it using csv reader to avoid mistaken splitting by commas in the data fields
-        #ip,date,time,cik,accession,extention
-        header = list(csv.reader([next(f)], delimiter=',', quotechar="'"))[0]
-        ip_in = header.index('ip')
-        time_c_in = header.index('time')
-        date_c_in = header.index('date')
-        row = list(csv.reader([next(f)], delimiter=',', quotechar="'"))[0]
-
-        #maintain a dictionary of active sessions
-        curr_active_sessions = dict()
-
-        #Initialize the current time to any random integer
-        current_timestamp = 0
-
+        #input and file will be given as argument
+        input_file = sys.argv[1]
+        out_file = sys.argv[3]
         try:
-            while(row != ''):
+            #open file in read mode
+            with open(input_file, "r") as f:
 
-                #process each line in file in a streaming manner
-                curr_active_sessions, current_timestamp = process_line(curr_active_sessions, row, ip_in, date_c_in, time_c_in, current_timestamp)
-
-                #read the next row
+                #extract all the required fields, read it using csv reader to avoid mistaken splitting by commas in the data fields
+                #ip,date,time,cik,accession,extention
+                header = list(csv.reader([next(f)], delimiter=',', quotechar="'"))[0]
+                ip_in = header.index('ip')
+                time_c_in = header.index('time')
+                date_c_in = header.index('date')
                 row = list(csv.reader([next(f)], delimiter=',', quotechar="'"))[0]
-        except:
-            pass
-    f.close()
+                row_len = len(row)
+                #maintain a dictionary of active sessions
+                active_sessions = dict()
 
-    #write the remaining logs into file
-    write_output(curr_active_sessions, out_file, None)    
+                #Initialize the current time to any random integer
+                current_timestamp = 0
+
+                try:
+                    while(row != ''):
+                        #checks if the length of row is same as header lenghth else does not process row
+                        if(len(row) == row_len):
+                            #process each line in file in a streaming manner
+                            active_sessions, current_timestamp = process_line(active_sessions, row, ip_in, date_c_in, time_c_in, current_timestamp, st)
+
+                            #read the next row
+                            row = list(csv.reader([next(f)], delimiter=',', quotechar="'"))[0]
+                        else:
+                            pass
+                except:
+                    pass
+            f.close()
+            #write the remaining logs into file
+            write_output(active_sessions, out_file, None, st)
+        except IOError:
+            print("There was an error opening input file!")
+    else:
+        print(st)    
 
 
 
